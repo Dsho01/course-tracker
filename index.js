df1a6bc4c3b34659c4a1f993e0c8412bda2d58a5
Jorge <jdesh147@gmail.com>
2017-02-25 14:23:14 -0500

    fixed some styling, added environment variables for database, and edited package.json for heroku-ready deployment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var db = require('./config/db');
 var mongoose = require('mongoose');
 var app = express();
 
+// TODO: use npm as only package manager, delete bower
 app.use(express.static(__dirname + '/public/'));
 
 app.use('/', routes);
@@ -15,12 +16,20 @@ app.listen(3000, function() {
     console.log('App listening to port 3000!');
 });
 
+// TODO: Delete this line
+if(db.user && db.pw) {
+    console.log("Username and password detected");
+    db.url = 'mongodb://' + db.user + ':' + db.pw + '@ds157549.mlab.com:57549/coursesu';
+} else {
+    console.log("No username or password detected");
+}
+
 mongoose.connect(db.url, function(error) {
     //TODO: send to 404 page when database connection cannot be established
     if(error) {
         //Here so program does not crash
         console.log("Caught Error: Database connection has failed!");
     } else {
-        console.log("Connection established to database!");
+        console.log("Connection established to database!" + db.url);
     }
 });
